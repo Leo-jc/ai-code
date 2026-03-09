6a0f677b4c82bcdcd3b2a031adf0162d84597fbf
jianchuanZeng <jianchuanz@foxmail.com>
2026-03-09 14:32:07 +0800

    初始化

---
+++ src/main/java/com/serain/serainaicode/SerainAiCodeApplication.java
@@ -0,0 +1,13 @@
+package com.serain.serainaicode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerainAiCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SerainAiCodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/serain/serainaicode/SerainAiCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serain.serainaicode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SerainAiCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
